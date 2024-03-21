45cd8d937973b081fcee4737bb93b70c59691ea8
Sylvia Boamah <sbsboamah@gmail.com>
2024-03-20 21:19:15 -0400

    start new project

---
+++ src/components/Game.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+export default class Game extends React.Component{
+    data = require('../assets/json/quotes.json');
+    jsonString = JSON.stringify(this.data); 
+    quotes = JSON.parse(this.jsonString);
+    quoteNumbers = []; 
+
+    componentDidMount() { 
+        for (let quote of this.quotes) {
+            this.quoteNumbers.push(quote.id);
+        }
+    }
+
+    getRandomNumber(max) {
+        return Math.floor(Math.random() * max)
+    }
+
+    render() {
+        return (
+            <>
+                <section>
+                    {this.quotes.length && <p>{this.quotes[this.getRandomNumber(this.quotes.length)].quote}</p>}
+                </section>
+                <a>Floyd</a>
+                <a>Freud</a>
+            </>
+        );
+      }
+}
